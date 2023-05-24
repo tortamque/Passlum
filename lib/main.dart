d25760d32ae3265754b0f5044ecfc00e0345252c
tortamque <90132962+tortamque@users.noreply.github.com>
2023-05-24 23:48:50 +0300

    Create placeholders

--- lib/main.dart
+++ lib/main.dart
@@ -30,14 +30,66 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
+    final screenWidth = MediaQuery.of(context).size.width;
+
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
-      body: SizedBox.expand(
-        
+      body: Padding(
+        padding: EdgeInsets.symmetric(
+          horizontal: screenWidth/25
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Expanded(
+              flex: 1,
+              child: Container(
+                height: 200,
+              ),
+            ),
+            Expanded(
+              flex: 3,
+              child: Container(
+                height: 200,
+                color: Colors.blue[800],
+              ),
+            ),
+            Expanded(
+              flex: 1,
+              child: Container(
+                height: 200,
+              ),
+            ),
+            Expanded(
+              flex: 12,
+              child: Container(
+                height: 200,
+                color: Colors.red[800],
+              ),
+            ),
+            Expanded(
+              flex: 1,
+              child: Container(
+                height: 200,
+              ),
+            ),
+            TextButton(
+              onPressed: (){},
+              child: Text("Copy password"),
+            ),
+            Expanded(
+              flex: 1,
+              child: Container(
+                height: 200,
+              ),
+            ),
+          ]
+        ),
       )
-      );
+    );
   }
 }
