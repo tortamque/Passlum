3ff9b988cf5519be7a71ae3b00f988948136103f
tortamque <90132962+tortamque@users.noreply.github.com>
2023-05-31 23:11:03 +0300

    Decompose StrenghtLine widget into multiple smaller

--- lib/widgets/password_field/strength_line.dart
+++ lib/widgets/password_field/strength_line.dart
@@ -23,44 +23,55 @@ class _StrenghtLineState extends State<StrenghtLine> {
       flex: widget.flex,
       child: BlocBuilder<PasswordBloc, String>(
         builder: (context, state) {
-          Strength passwordStrength = _checkPasswordStrength(state);
-
-          double widthFraction;
-          if (passwordStrength == Strength.weak) {
-            widthFraction = 1 / 3;
-          } else if (passwordStrength == Strength.medium) {
-            widthFraction = 2 / 3;
-          } else {
-            widthFraction = 1.0;
-          }
-
-          return AnimatedContainer(
-            duration: const Duration(milliseconds: 250),
-            curve: Curves.easeInOut,
-            margin: const EdgeInsets.all(0),
-            decoration: BoxDecoration(
-              color: () {
-                if (passwordStrength == Strength.strong) {
-                  return CustomColors.strengthLineGreen;
-                } else if (passwordStrength == Strength.medium) {
-                  return CustomColors.strengthLineYellow;
-                } else {
-                  return CustomColors.strengthLineRed;
-                }
-              }(),
-              borderRadius: passwordStrength == Strength.strong
-                  ? const BorderRadius.only(
-                      bottomLeft: Radius.circular(10),
-                      bottomRight: Radius.circular(10),
-                    )
-                  : const BorderRadius.all(Radius.circular(10)),
-            ),
-            width: MediaQuery.of(context).size.width * widthFraction,
-          );
+          return _buildStrengthLine(context, state);
         },
       ),
     );
   }
+}
+
+
+Widget _buildStrengthLine(BuildContext context, String state) {
+  Strength passwordStrength = _checkPasswordStrength(state);
+
+  double widthFraction;
+  if (passwordStrength == Strength.weak) {
+    widthFraction = 1 / 3;
+  } else if (passwordStrength == Strength.medium) {
+    widthFraction = 2 / 3;
+  } else {
+    widthFraction = 1.0;
+  }
+
+  return AnimatedContainer(
+    duration: const Duration(milliseconds: 250),
+    curve: Curves.easeInOut,
+    margin: const EdgeInsets.all(0),
+    decoration: BoxDecoration(
+      color: _getStrengthLineColor(passwordStrength),
+      borderRadius: _getStrengthLineBorderRadius(passwordStrength),
+    ),
+    width: MediaQuery.of(context).size.width * widthFraction,
+  );
+}
+
+Color _getStrengthLineColor(Strength passwordStrength) {
+  if (passwordStrength == Strength.strong) {
+    return CustomColors.strengthLineGreen;
+  } else if (passwordStrength == Strength.medium) {
+    return CustomColors.strengthLineYellow;
+  } else {
+    return CustomColors.strengthLineRed;
+  }
+}
+
+BorderRadius _getStrengthLineBorderRadius(Strength passwordStrength) {
+  return passwordStrength == Strength.strong
+      ? const BorderRadius.only(
+          bottomLeft: Radius.circular(10),
+          bottomRight: Radius.circular(10),
+        )
+      : const BorderRadius.all(Radius.circular(10));
 }
 
 Strength _checkPasswordStrength(String password) {
