cddc77e509036effd3e1c34a93f4fd1abc04a0af
tortamque <90132962+tortamque@users.noreply.github.com>
2023-05-31 23:08:25 +0300

    Add width change animation for strength line

--- lib/widgets/password_field/strength_line.dart
+++ lib/widgets/password_field/strength_line.dart
@@ -3,7 +3,6 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:passlum/business_logic/password_bloc.dart';
 import 'package:passlum/enums/password_type.dart';
 import 'package:passlum/theme/constants.dart';
-import 'package:passlum/widgets/password_settings/password_lenght_form/password_lenght_form.dart';
 
 class StrenghtLine extends StatefulWidget {
   final int flex;
@@ -35,35 +34,31 @@ class _StrenghtLineState extends State<StrenghtLine> {
             widthFraction = 1.0;
           }
 
-          return FractionallySizedBox(
-            widthFactor: widthFraction,
-            child: AnimatedContainer(
-              duration: const Duration(milliseconds: 250),
-              curve: Curves.easeInOut,
-              margin: const EdgeInsets.all(0),
-              decoration: BoxDecoration(
-                color: () {
-                  if (passwordStrength == Strength.strong) {
-                    return CustomColors.strengthLineGreen;
-                  } else if (passwordStrength == Strength.medium) {
-                    return CustomColors.strengthLineYellow;
-                  } else {
-                    return CustomColors.strengthLineRed;
-                  }
-                }(),
-                borderRadius: passwordStrength == Strength.strong ? 
-                  const BorderRadius.only(
-                    bottomLeft: Radius.circular(10),
-                    bottomRight: Radius.circular(10),
-                  )
-                  : const BorderRadius.all(
-                    Radius.circular(10)
-                  )
-              ),
+          return AnimatedContainer(
+            duration: const Duration(milliseconds: 250),
+            curve: Curves.easeInOut,
+            margin: const EdgeInsets.all(0),
+            decoration: BoxDecoration(
+              color: () {
+                if (passwordStrength == Strength.strong) {
+                  return CustomColors.strengthLineGreen;
+                } else if (passwordStrength == Strength.medium) {
+                  return CustomColors.strengthLineYellow;
+                } else {
+                  return CustomColors.strengthLineRed;
+                }
+              }(),
+              borderRadius: passwordStrength == Strength.strong
+                  ? const BorderRadius.only(
+                      bottomLeft: Radius.circular(10),
+                      bottomRight: Radius.circular(10),
+                    )
+                  : const BorderRadius.all(Radius.circular(10)),
             ),
+            width: MediaQuery.of(context).size.width * widthFraction,
           );
         },
-      )
+      ),
     );
   }
 }
@@ -85,7 +80,7 @@ Strength _checkPasswordStrength(String password) {
 
   if (uppercaseCount >= 2 && numberCount >= 1 && specialCharCount >= 1 && password.length >= 12) {
     return Strength.strong;
-  } else if ((uppercaseCount >= 2 && numberCount >= 1) || specialCharCount >= 1) {
+  } else if (((uppercaseCount >= 2 && numberCount >= 1) || specialCharCount >= 1) && password.length >= 8) {
     return Strength.medium;
   } else {
     return Strength.weak;
