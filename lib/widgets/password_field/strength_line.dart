c181060117155da719aa13d6ecfeb5247c7ceefc
tortamque <90132962+tortamque@users.noreply.github.com>
2023-05-31 22:56:33 +0300

    Add width change for strength line

--- lib/widgets/password_field/strength_line.dart
+++ lib/widgets/password_field/strength_line.dart
@@ -3,6 +3,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:passlum/business_logic/password_bloc.dart';
 import 'package:passlum/enums/password_type.dart';
 import 'package:passlum/theme/constants.dart';
+import 'package:passlum/widgets/password_settings/password_lenght_form/password_lenght_form.dart';
 
 class StrenghtLine extends StatefulWidget {
   final int flex;
@@ -22,31 +23,47 @@ class _StrenghtLineState extends State<StrenghtLine> {
     return Expanded(
       flex: widget.flex,
       child: BlocBuilder<PasswordBloc, String>(
-        builder: (context, state){
+        builder: (context, state) {
           Strength passwordStrength = _checkPasswordStrength(state);
-          return AnimatedContainer(
-            duration: const Duration(milliseconds: 250), 
-            curve: Curves.easeInOut, 
-            margin: const EdgeInsets.all(0),
-            width: double.infinity,
-            decoration: BoxDecoration(
-              color: () {
-                if (passwordStrength == Strength.strong) {
-                  return CustomColors.strengthLineGreen;
-                } else if (passwordStrength == Strength.medium) {
-                  return CustomColors.strengthLineYellow;
-                } else {
-                  return CustomColors.strengthLineRed;
-                }
-              }(),
-              borderRadius: const BorderRadius.only(
-                bottomLeft: Radius.circular(10),
-                bottomRight: Radius.circular(10),
+
+          double widthFraction;
+          if (passwordStrength == Strength.weak) {
+            widthFraction = 1 / 3;
+          } else if (passwordStrength == Strength.medium) {
+            widthFraction = 2 / 3;
+          } else {
+            widthFraction = 1.0;
+          }
+
+          return FractionallySizedBox(
+            widthFactor: widthFraction,
+            child: AnimatedContainer(
+              duration: const Duration(milliseconds: 250),
+              curve: Curves.easeInOut,
+              margin: const EdgeInsets.all(0),
+              decoration: BoxDecoration(
+                color: () {
+                  if (passwordStrength == Strength.strong) {
+                    return CustomColors.strengthLineGreen;
+                  } else if (passwordStrength == Strength.medium) {
+                    return CustomColors.strengthLineYellow;
+                  } else {
+                    return CustomColors.strengthLineRed;
+                  }
+                }(),
+                borderRadius: passwordStrength == Strength.strong ? 
+                  const BorderRadius.only(
+                    bottomLeft: Radius.circular(10),
+                    bottomRight: Radius.circular(10),
+                  )
+                  : const BorderRadius.all(
+                    Radius.circular(10)
+                  )
               ),
             ),
           );
-        }
-      ),
+        },
+      )
     );
   }
 }
@@ -66,7 +83,7 @@ Strength _checkPasswordStrength(String password) {
     }
   }
 
-  if (uppercaseCount >= 2 && numberCount >= 1 && specialCharCount >= 1) {
+  if (uppercaseCount >= 2 && numberCount >= 1 && specialCharCount >= 1 && password.length >= 12) {
     return Strength.strong;
   } else if ((uppercaseCount >= 2 && numberCount >= 1) || specialCharCount >= 1) {
     return Strength.medium;
